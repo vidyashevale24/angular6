71e2444f216748a03f65bca0dcf7750f7926930a
password4gitlab <vidya.sheval24@gmail.com>
2018-09-08 14:08:41 +0530

    Added login,register,home,users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes , RouterModule, Router} from '@angular/router';
 
+const r: Routes[
+  {path: '', component: HomeComponent},
+  {path: 'Users', component: UsersComponent},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Register', component: RegisterComponent}
+ ];
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule .forRoot(r)
   ],
   providers: [],
   bootstrap: [AppComponent]
