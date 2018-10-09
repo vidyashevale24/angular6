4eeb361529cd9ac58c9f911d8ab694a0594f1057
password4gitlab <vidya.sheval24@gmail.com>
2018-10-09 22:56:20 +0530

    modified login file

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { FormGroup , FormBuilder , Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,8 +9,13 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.Native
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm :FormGroup;
+  constructor(private  _fb: FormBuilder) {
+      this.loginForm = this._fb.group({
+          username: ['Vidya', [Validators.required , Validators.pattern('[A-Za-z0-9]{3,}[@][A-Za-z0-9]{2,}[.][A-Za-z0-9]{2,}')]],
+          password: ['123', [Validators.required]]
+      });
+  }
 
   ngOnInit() {
   }
